a62a6f7f2a4d9c6238662eaff8fd76d4ce24a0b1
Szabó Péter Levente <levente1001@TARTOMANY.FAZEKAS.HU>
2018-01-16 18:06:56 +0100

    feltoltottem!!!

---
+++ peti/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        //int[][] peti = new int[][]{
+        //        {0,1,1,0,1,0,1,0,1,0},
+        //        {1,0,0,1,0,1,0,1,1,1},
+        //        {1,0,0,0,1,1,1,0,1,0},
+        //        {0,1,1,0,1,0,1,0,1,0},
+        //        {1,0,0,0,1,1,1,0,1,0},
+        //        {1,0,0,0,1,1,1,0,1,0},
+        //        {0,1,1,0,1,0,1,0,1,0},
+        //        {1,0,0,0,1,1,1,0,1,0},
+        //        {0,1,1,0,1,0,1,0,1,0},
+        //        {1,0,0,0,1,1,1,0,1,0},
+        //};
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[][] tömb = new int[N][N];
+        for(i = 0; i<= N; i++){
+            for(a = 0; a<=N; a++){
+                x = 0;
+                if(i = x | a = x){
+                    
+                }
+            }
+        }
+    }
+}
